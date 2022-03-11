15a4bd5002628a16a3d7b7914e1876d0dae3f029
vivekgr92 <vivekgr92@gmail.com>
2022-03-10 16:26:07 -0800

    Add Visualizations

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -2,6 +2,7 @@ import sys
 sys.path.append('../')
 import numpy as np
 import torch
+import wandb
 
 
 import torchvision
@@ -13,6 +14,26 @@ from utils import utils
 from features import build_features, transforms as T
 import pdb
 import matplotlib.pyplot as plt
+
+
+######## WANDB integration begins (mainly) here
+
+optim_config = dict(
+    lr=0.005,
+    momentum=0.9,
+    weight_decay=0.0005,
+)
+lr_config = dict(
+    step_size=3,
+    gamma=0.1
+)
+config = dict( # TODO: assert no params are overriden
+    num_epochs=1,
+    **optim_config,
+    **lr_config,
+)
+
+
 
 
 def get_model_instance_segmentation(num_classes):
@@ -55,46 +76,74 @@ num_classes = 1 + 3
 dataset = build_features.AmyBDataset('/home/vivek/Datasets/mask_rcnn/dataset/train', get_transform(train=True))
 dataset_test = build_features.AmyBDataset('/home/vivek/Datasets/mask_rcnn/dataset/val', get_transform(train=False))
 
-# define training and validation data loaders
-data_loader = torch.utils.data.DataLoader(
-    dataset, batch_size=1, shuffle=True, num_workers=4,
-    collate_fn=utils.collate_fn)
 
-data_loader_test = torch.utils.data.DataLoader(
-    dataset_test, batch_size=1, shuffle=False, num_workers=4,
-    collate_fn=utils.collate_fn)
+with wandb.init(project='amyb-plaque-detection', config=config):
 
-# get the model using our helper function
-model = get_model_instance_segmentation(num_classes)
+    # define training and validation data loaders
+    data_loader = torch.utils.data.DataLoader(
+        dataset, batch_size=1, shuffle=True, num_workers=4,
+        collate_fn=utils.collate_fn)
 
-# move model to the right device
-model.to(device)
+    data_loader_test = torch.utils.data.DataLoader(
+        dataset_test, batch_size=1, shuffle=False, num_workers=4,
+        collate_fn=utils.collate_fn)
 
-# construct an optimizer
-params = [p for p in model.parameters() if p.requires_grad]
-# optimizer = torch.optim.SGD(params, lr=0.001,
-                            # momentum=0.9)
-optimizer = torch.optim.Adam(params,lr=0.00005,betas=(0.9,0.999),eps=1e-08,weight_decay=0,amsgrad=False)
-# and a learning rate scheduler
-# lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
-#                                                 step_size=3,
-#    
-# lr                                             gamma=0.1)
-lr_scheduler = None
+    # get the model using our helper function
+    model = get_model_instance_segmentation(num_classes)
 
-# let's train it for 10 epochs
-num_epochs = 6
+    # move model to the right device
+    model.to(device)
 
-for epoch in range(num_epochs):
-    # train for one epoch, printing every 10 iterations
-    train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=20)
-    # update the learning rate
-    # lr_scheduler.step()
-    # evaluate on the test dataset
+    # construct an optimizer
+    params = [p for p in model.parameters() if p.requires_grad]
+    optimizer = torch.optim.SGD(params, **optim_config)
+    # and a learning rate scheduler
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, **lr_config)
+   
 
-    # evaluate(model, data_loader_test, device=device)
+    for epoch in range(config['num_epochs']):
+        train_logs = train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=1)
+        # update the learning rate
+        lr_scheduler.step()
+        # evaluate on the test dataset
+        # eval_logs, eval_res = evaluate(model, data_loader_test, device=device)
 
-model_save_name = "../../models/mrcnn_model_{epoch:}.pth"
-torch.save(model, model_save_name.format(epoch=num_epochs))
+        # train_logs are instances of utils.engine.MetricLogger,
+        #   which stores metrics in train_logs.meters, an instance of
+        #   utils.utils.SmoothedValue. The object train_metrics.deque
+        #   contains per-batch results of the training, while providing
+        #   smoothed sliding-window views of the metrics across training runs
+        train_metrics = train_logs.meters
+        # eval_metrics = eval_logs.meters
 
-print("The Model is Trained!")
+        # eval_res is an isntance of utils.coco_eval.CocoEvaluator,
+        #   which stores the the instance ids of images passed to it in
+        #   eval_res.img_ids, and processes instances as one of several
+        #   eval_res.iou_types. The dictionary eval_res.eval_imgs stores,
+        #   with eval_res.iou_types as keys, the results of an evaluation run.
+        # eval_imgs = eval_res.eval_imgs
+        train_metrics_dict = {k: list(v.deque) for k, v in train_metrics.items() if k!='lr'}
+        
+        d = train_metrics_dict
+        keys, vals = tuple(zip(*d.items()))
+        unpacked = [dict(zip(keys, v)) for v in list(zip(*vals))] 
+        pdb.set_trace()
+
+        
+
+        wandb.log({'loss': train_metrics['loss'].global_avg, 
+                   'loss_classifier': train_metrics['loss_classifier'].global_avg,
+                   'loss_rpn_box_reg': train_metrics['loss_rpn_box_reg'].global_avg,
+                   'loss_box_reg': train_metrics['loss_box_reg'].global_avg,
+                   'loss_mask': train_metrics['loss_mask'].global_avg,
+                   'loss_objectness': train_metrics['loss_objectness'].global_avg,
+                    'epoch': epoch,
+                    'data_point_details':unpacked})
+            
+        # wandb.log(train_metrics_dict)
+
+    model_save_name = "../../models/mrcnn_model_{epoch:}.pth"
+    torch.save(model, model_save_name.format(epoch=config['num_epochs']))
+
+    print("The Model is Trained!")
+    wandb.finish()
