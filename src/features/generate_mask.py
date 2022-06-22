04b320eb81bd54c91c7c38908ef53f90833ec509
vivekgr92 <vivekgr92@gmail.com>
2022-06-22 12:37:52 -0700

    use vips crop to tile

--- src/features/generate_mask.py
+++ src/features/generate_mask.py
@@ -19,6 +19,8 @@ import pyfiglet
 from skimage import measure
 from tqdm import tqdm
 from PIL import Image
+import pyvips as Vips
+import openslide
 
 # Mask size should be same as image size
 ID_MASK_SHAPE = (224, 224)
@@ -27,9 +29,8 @@ ID_MASK_SHAPE = (224, 224)
 lablel2id = {'Core':'50', 'Diffused':'100',
              'Neuritic':'150', 'Unknown':'250'}
 
-def save_mask(mask, file_name, tileX, tileY, save_dir, label="mask"):
-    im = Image.fromarray(mask)
-    pdb.set_trace()
+def save_img(img, file_name, tileX, tileY, save_dir, label="mask"):
+    im = Image.fromarray(img)
 
     file_name = file_name + "_" + str(tileX)+"x" + "_" + str(tileY) + "y" + "_" + label + ".png"
    
@@ -43,7 +44,6 @@ def polygon2id(image_shape, mask, ids, coords_x, coords_y):
         vertex_row_coords, vertex_col_coords, image_shape)
     
     # Row and col are flipped
-    pdb.set_trace()
     mask[fill_col_coords, fill_row_coords] = ids
     return mask
 
@@ -73,7 +73,21 @@ def polygon2mask(image_shape, mask, color, coords_x, coords_y):
     # mask[fill_row_coords, fill_col_coords] = color
     return mask
 
-def process_json(json_path, save_dir, visualize=False):
+def cropTiles(tileX, tileY, width, height): {
+
+
+}
+
+def get_vips_info(vips_img):
+    # # Get bounds-x and bounds-y offeset
+    vfields = [f.split('.') for f in vips_img.get_fields()]
+    vfields = [f for f in vfields if f[0] == 'openslide']
+    vfields = dict([('.'.join(k[1:]), vips_img.get('.'.join(k))) for k in vfields])
+    
+    return vfields
+
+
+def process_json(json_path, imagename,  save_dir, visualize=False):
     """This function is used to read and process the json files
     and generate save generated masks
     
@@ -83,9 +97,23 @@ def process_json(json_path, save_dir, visualize=False):
     save_dir : dir where the generated masks will be saved
     visualize : True , if you want to see the mask generated
     """
-   
-    if not os.path.exists(save_dir):
-        os.makedirs(save_dir)
+    
+
+    # Mask Folder
+    mask_save_dir = os.path.join(save_dir, "masks")
+    if not os.path.exists(mask_save_dir):
+        os.makedirs(mask_save_dir)
+    
+    # Image Folder
+    image_save_dir = os.path.join(save_dir, "images")
+    if not os.path.exists(image_save_dir):
+        os.makedirs(image_save_dir)
+
+    # Read the WSI image
+    vips_img = Vips.Image.new_from_file(imagename, level=0)
+    vinfo = get_vips_info(vips_img)
+
+    
 
     json_files = glob.glob(os.path.join(json_path, "*.json"))
     for json_file in json_files:
@@ -107,10 +135,23 @@ def process_json(json_path, save_dir, visualize=False):
             i = 0
            
             for region in ele['region_attributes']:
+
                 
                 # Get tileX and tileY
                 tileX = region['tiles'][0]['tileId'][0]
                 tileY = region['tiles'][0]['tileId'][1]
+                tileWidth = region['tiles'][0]['tileBounds']["WH"][0]
+                tileHeight = region['tiles'][0]['tileBounds']["WH"][1]
+
+                # crop the image
+                # get the bound-x and bounds-y, offset as Vips crops the empty spaces. Qupath does not
+                tileX = (tileX * tileWidth) + int(vinfo['bounds-x'])
+                tileY = (tileY * tileHeight) + int(vinfo['bounds-y'])
+
+                vips_img = vips_img.crop(tileX, tileY,tileWidth, tileHeight)
+                vips_img = np.ndarray(buffer=vips_img.write_to_memory(), dtype=np.uint8, 
+                                      shape=(vips_img.height, vips_img.width, vips_img.bands))[..., :3]
+
 
                 # unpack from [x,y] to [x], [y]
                 coords_x, coords_y = zip(*region['points'])
@@ -134,7 +175,9 @@ def process_json(json_path, save_dir, visualize=False):
 
                 i+=1
 
-            save_mask(id_mask, ele['filename'], tileX, tileY, save_dir)
+        
+            save_img(vips_img, ele['filename'], tileX, tileY, image_save_dir, "image")
+            save_img(id_mask, ele['filename'], tileX, tileY, mask_save_dir,"mask")
 
                 
     
@@ -151,11 +194,13 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Generate Mask')
     parser.add_argument('json_path',
                         help='Enter the path annotation json file resides')
+    parser.add_argument('WSI_path',
+                        help='Enter the path where WSI resides')
     parser.add_argument('save_dir',
                         help='Enter the path where you want to save image masks')
 
     args = parser.parse_args()
 
-    process_json(args.json_path, args.save_dir)
+    process_json(args.json_path, args.WSI_path, args.save_dir)
 
 
