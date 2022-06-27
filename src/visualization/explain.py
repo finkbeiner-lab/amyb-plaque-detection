16647555333e7da3e74a95bc9c3e2c2470cfc3f0
vivekgr92 <vivekgr92@gmail.com>
2022-06-26 17:13:38 -0700

    Add class template

--- src/visualization/explain.py
+++ src/visualization/explain.py
@@ -21,138 +21,108 @@ from pytorch_grad_cam.ablation_layer import AblationLayerFasterRCNN
 import random
 import glob
 
-THRESHOLD = 0.75
 
-def get_outputs(input_tensor, model, threshold):
-    with torch.no_grad():
-        # forward pass of the image through the modle
-        outputs = model(input_tensor)
+class ExplainPredictions():
+
+    def __init__(self, model_input_path, test_input_path, detection_threshold, ):
+        self.model_input_path = model_input_path
+        self.test_input_path = test_input_path
+        self.detection_threshold = detection_threshold
+        self.class_names = ['Unknown', 'Core', 'Diffuse', 'Neuritic']
+
+
+    def get_outputs(self, input_tensor, model, threshold):
+        with torch.no_grad():
+            # forward pass of the image through the modle
+            outputs = model(input_tensor)
+        
+        # get all the scores
+        scores = list(outputs[0]['scores'].detach().cpu().numpy())
+        print("\n scores", max(scores))
+        # index of those scores which are above a certain threshold
+        thresholded_preds_inidices = [scores.index(i) for i in scores if i > threshold]
+        thresholded_preds_count = len(thresholded_preds_inidices)
+        # get the masks
+        masks = (outputs[0]['masks']>0.5).squeeze().detach().cpu().numpy()
+        # print("masks", masks)
+        # discard masks for objects which are below threshold
+        masks = masks[:thresholded_preds_count]
+        # get the bounding boxes, in (x1, y1), (x2, y2) format
+        boxes = [[(int(i[0]), int(i[1])), (int(i[2]), int(i[3]))]  for i in outputs[0]['boxes'].detach().cpu()]
+        # discard bounding boxes below threshold value
+        boxes = boxes[:thresholded_preds_count]
+        # get the classes labels
+        # print('labels', outputs[0]['labels'])
+        labels = [coco_names[i] for i in outputs[0]['labels']]
+
+        # [1,1,1, 2, 2, 2, 3, 3]
+        return masks, boxes, labels
+
+    def draw_segmentation_map(self, image, masks, boxes, labels):
+        alpha = 1 
+        beta = 0.6 # transparency for the segmentation map
+        gamma = 0 # scalar added to each 
+        segmentation_map = np.zeros((image.shape[0], image.shape[1]), dtype=np.uint8)
     
-    # get all the scores
-    scores = list(outputs[0]['scores'].detach().cpu().numpy())
-    print("\n scores", max(scores))
-    # index of those scores which are above a certain threshold
-    thresholded_preds_inidices = [scores.index(i) for i in scores if i > threshold]
-    thresholded_preds_count = len(thresholded_preds_inidices)
-    # get the masks
-    masks = (outputs[0]['masks']>0.5).squeeze().detach().cpu().numpy()
-    # print("masks", masks)
-    # discard masks for objects which are below threshold
-    masks = masks[:thresholded_preds_count]
-    # get the bounding boxes, in (x1, y1), (x2, y2) format
-    boxes = [[(int(i[0]), int(i[1])), (int(i[2]), int(i[3]))]  for i in outputs[0]['boxes'].detach().cpu()]
-    # discard bounding boxes below threshold value
-    boxes = boxes[:thresholded_preds_count]
-    # get the classes labels
-    # print('labels', outputs[0]['labels'])
-    labels = [coco_names[i] for i in outputs[0]['labels']]
+        for i in range(len(masks)):
+            red_map = np.zeros_like(masks[i]).astype(np.uint8)
+            green_map = np.zeros_like(masks[i]).astype(np.uint8)
+            blue_map = np.zeros_like(masks[i]).astype(np.uint8)
+            
+            # apply a randon color mask to each object
+            rect_color = (0,0,0)
+            color = COLORS[random.randrange(0, len(COLORS))]
+            red_map[masks[i] == 1], green_map[masks[i] == 1], blue_map[masks[i] == 1]  = color
+            # combine all the masks into a single image
+            # change the format of mask to W,H, C
 
-    # [1,1,1, 2, 2, 2, 3, 3]
-    return masks, boxes, labels
+            segmentation_map = np.stack([red_map, green_map, blue_map], axis=2)
+            #convert the original PIL image into NumPy format
+            image = np.array(image)
+            # convert from RGB to OpenCV BGR format
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            # apply mask on the image
+            # cv2.addWeighted(image, alpha, segmentation_map, beta, gamma, image)
+            # draw the bounding boxes around the objects
+            cv2.rectangle(image, boxes[i][0], boxes[i][1], color=rect_color, 
+                        thickness=2)
+            # Get the centre coords of the rectangle
+            x1 = boxes[i][0][0]
+            y1 = boxes[i][0][1]
+            x2 = boxes[i][1][0]
+            y2 = boxes[i][1][1]
+            x = int((x1 + x2) / 2)
+            y = int((y1+y2) / 2)
 
-def draw_segmentation_map(image, masks, boxes, labels):
-    alpha = 1 
-    beta = 0.6 # transparency for the segmentation map
-    gamma = 0 # scalar added to each 
-    segmentation_map = np.zeros((image.shape[0], image.shape[1]), dtype=np.uint8)
-   
-    for i in range(len(masks)):
-        red_map = np.zeros_like(masks[i]).astype(np.uint8)
-        green_map = np.zeros_like(masks[i]).astype(np.uint8)
-        blue_map = np.zeros_like(masks[i]).astype(np.uint8)
-        
-        # apply a randon color mask to each object
-        rect_color = (0,0,0)
-        color = COLORS[random.randrange(0, len(COLORS))]
-        red_map[masks[i] == 1], green_map[masks[i] == 1], blue_map[masks[i] == 1]  = color
-        # combine all the masks into a single image
-        # change the format of mask to W,H, C
+            
+            # put the label text above the objects
+            cv2.putText(image , labels[i], (x1, y1-20), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, color, 
+                        thickness=2, lineType=cv2.LINE_AA)
+            
+            # Convert Back
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        return image, segmentation_map
 
-        segmentation_map = np.stack([red_map, green_map, blue_map], axis=2)
-        #convert the original PIL image into NumPy format
-        image = np.array(image)
-        # convert from RGB to OpenCV BGR format
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        # apply mask on the image
-        # cv2.addWeighted(image, alpha, segmentation_map, beta, gamma, image)
-        # draw the bounding boxes around the objects
-        cv2.rectangle(image, boxes[i][0], boxes[i][1], color=rect_color, 
-                      thickness=2)
-        # Get the centre coords of the rectangle
-        x1 = boxes[i][0][0]
-        y1 = boxes[i][0][1]
-        x2 = boxes[i][1][0]
-        y2 = boxes[i][1][1]
-        x = int((x1 + x2) / 2)
-        y = int((y1+y2) / 2)
-
-        
-        # put the label text above the objects
-        cv2.putText(image , labels[i], (x1, y1-20), 
-                    cv2.FONT_HERSHEY_SIMPLEX, 1, color, 
-                    thickness=2, lineType=cv2.LINE_AA)
-        
-        # Convert Back
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    return image, segmentation_map
-
-
-def predict(input_tensor, model, device, detection_threshold):
-    outputs = model(input_tensor)
-    # i- 1 zero indexing - the model outputs a non zero indexing format ( 1, 2, 3)
-    pred_classes = [coco_names[i] for i in outputs[0]['labels'].cpu().numpy()]
-    pred_labels = outputs[0]['labels'].cpu().numpy()
-    pred_scores = outputs[0]['scores'].detach().cpu().numpy()
-    pred_bboxes = outputs[0]['boxes'].detach().cpu().numpy()
+    def prepare_input(self, image):
     
-    boxes, classes, labels, indices = [], [], [], []
-    for index in range(len(pred_scores)):
-        if pred_scores[index] >= detection_threshold:
-            boxes.append(pred_bboxes[index].astype(np.int32))
-            classes.append(pred_classes[index])
-            labels.append(pred_labels[index])
-            indices.append(index)
-    boxes = np.int32(boxes)
-    return boxes, classes, labels, indices
+        image_float_np = np.float32(image) / 255
 
-def draw_boxes(boxes, labels, classes, image):
-    for i, box in enumerate(boxes):
-        color = COLORS[labels[i]]
-        cv2.rectangle(
-            image,
-            (int(box[0]), int(box[1])),
-            (int(box[2]), int(box[3])),
-            color, 2
-        )
-        cv2.putText(image, classes[i], (int(box[0]), int(box[1] + 30)),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 1,
-                    lineType=cv2.LINE_AA)
-    return image
+        # define the torchvision image transforms
+        transform = torchvision.transforms.Compose([
+            torchvision.transforms.ToTensor(),
+        ])
 
+        input_tensor = transform(image)
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        input_tensor = input_tensor.to(device)
+        # Add a batch dimension:
+        input_tensor = input_tensor.unsqueeze(0)
 
-def prepare_input(image):
-   
-    image_float_np = np.float32(image) / 255
-
-    # define the torchvision image transforms
-    transform = torchvision.transforms.Compose([
-        torchvision.transforms.ToTensor(),
-    ])
-
-    input_tensor = transform(image)
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    input_tensor = input_tensor.to(device)
-    # Add a batch dimension:
-    input_tensor = input_tensor.unsqueeze(0)
-
-    return input_tensor, image_float_np
+        return input_tensor, image_float_np
 
 
 
-coco_names = ['Unknown', 'Core', 'Diffuse', 'Neuritic']
-
-# TODO Change to Val 
-input_path = '/home/vivek/Datasets/AmyB/amyb_wsi/test/images'
 
 # This will help us create a different color for each class
 COLORS = np.random.uniform(0, 255, size=(len(coco_names), 3))
@@ -227,3 +197,10 @@ for img in images:
     plt.savefig(save_path_ablation.format(no=i))
     i = i + 1
     # # plt.show()
+
+if __name__ == "__main__":
+
+    input_path = '/home/vivek/Datasets/AmyB/amyb_wsi/test/images'
+    explain = ExplainPredictions(test_input_path=input_path, detection_threshold=0.75,)
+
+
