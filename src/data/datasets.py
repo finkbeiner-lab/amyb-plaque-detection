47cd6a7b15e04c7a8298271b531c64031262271a
Gennadi Ryan <gennadiryan@gmail.com>
2022-12-12 17:18:55 -0800

    Implementing instance filter-by-box and filter-by-mask

--- src/data/datasets.py
+++ src/data/datasets.py
@@ -53,8 +53,20 @@ def get_tile(
     step: Tuple[int, int],
     size: Tuple[int, int],
     offset: Tuple[int, int],
-) -> Tuple[int, int, int, int]:
-    return tuple([(_tile * _step) + (i * _size) + _offset for i in range(2) for _tile, _step, _size, _offset in zip(tile, step, size, offset)])
+) -> np.ndarray:
+    return np.array(tuple([(_tile * _step) + (i * (_size - 1)) + _offset for i in range(2) for _tile, _step, _size, _offset in zip(tile, step, size, offset)]))
+
+def get_clipped(
+    tile: np.ndarray,
+    box: np.ndarray,
+) -> Tuple[np.ndarray, np.ndarray]:
+    clipped = box.copy()
+    clipped[:2] = np.maximum(tile[:2], clipped[:2]) - tile[:2]
+    clipped[2:] = np.minimum(tile[2:], clipped[2:]) - tile[:2]
+
+    offset = clipped + np.concatenate([tile[:2] - box[:2]] * 2)
+
+    return clipped, offset
 
 
 class TileDataset(Dataset):
@@ -78,7 +90,7 @@ class TileDataset(Dataset):
     def __getitem__(
         self,
         idx: int,
-    ) -> Tuple[int, int, int, int]:
+    ) -> np.ndarray:
         return get_tile(self.tiles[idx], self.step, self.size, self.offset)
         # return np.array(get_tile(self.tiles[idx], self.step, self.size, self.offset))
 
@@ -102,13 +114,22 @@ class VipsDataset(TileDataset):
             **kwargs,
         )
 
+    @staticmethod
+    def read_vips(vips_img, tile):
+        x, y = tile[:2]
+        w, h = (tile[2:] - tile[:2]) + 1
+        return vips_img.crop(x, y, w, h).numpy()
+
     def __getitem__(
         self,
         idx: int,
     ) -> Any:
-        x1, y1, x2, y2 = super().__getitem__(idx)
-        vips_crop = self.vips_img.crop(x1, y1, x2 - x1, y2 - y1)
-        return vips_crop.numpy()
+        return VipsDataset.read_vips(self.vips_img, super().__getitem__(idx))
+        # tile = super().__getitem__(idx)
+        # x, y = tile[:2]
+        # w, h = (tile[2:] - tile[:2]) + 1
+        # vips_crop = self.vips_img.crop(x, y, w, h)
+        # return vips_crop.numpy()
         # return np.ndarray(
         #     buffer=vips_crop.write_to_memory(),
         #     dtype=np.uint8,
@@ -123,23 +144,83 @@ class JsonDataset(TileDataset):
         label_names: List[str],
         **kwargs,
     ) -> None:
-        # super().__init__(**kwargs)
+        super().__init__(**kwargs)
         with open(json_name, 'r') as f:
-            self.json_obj = json.loads(f.read())
-        self.annotation_obj = self.json_obj.get('annotations')
-        self.tile_obj = self.json_obj.get('tiles')
+            self.json = json.loads(f.read())
+
+        self.labels, self.boxes, self.masks = zip(*map(lambda _: JsonDataset.read_json(_, label_names), self.json.get('annotations')))
+
+        self.tile_map = OrderedDict()
+        for i, (box, mask) in enumerate(zip(self.boxes, self.masks)):
+            for tile in tiles_per_box(box, self.step, self.size, self.offset):
+                self.tile_map.setdefault(tile, list()).append(i)
+        self.tiles = list(self.tile_map.keys())
 
     @staticmethod
     def read_json(json_obj, label_names):
         label = np.array(1 + label_names.index(json_obj.get('pathClasses')[0])).astype(np.intc)
-        points = [np.array(json_obj.get(ax)).astype(np.intc) for ax in 'xy']
-        bbox = [f(ax) for f in (np.amin, np.amax) for ax in points]
-        mask = np.zeros((bbox[2:] - bbox[:2])[::-1]).astype(np.uint8)
 
-        
+        points = [np.array(json_obj.get(ax)).astype(np.intc) for ax in 'xy']
+        bbox = np.array([f(ax) for f in (np.amin, np.amax) for ax in points])
+
+        x, y = bbox[:2]
+        w, h = (bbox[2:] - bbox[:2]) + 1
+        xx, yy = polygon(points[0] - x, points[1] - y)
+
+        mask = np.zeros((h, w)).astype(np.uint8)
+        mask[yy, xx] = 1
+
+        return label, bbox, mask
+
+    def __getitem__(
+        self,
+        idx: int,
+    ) -> Any:
+        tile, id = super().__getitem__(idx), self.tiles[idx]
+
+        labels, boxes, masks = list(), list(), list()
+        for label, box, mask in [[a[i] for a in [self.labels, self.boxes, self.masks]] for i in self.tile_map[id]]:
+            labels.append(label)
+            box_clipped, box_offset = get_clipped(tile, box)
+            boxes.append(box_clipped)
+            masks.append(mask[box_offset[1]:box_offset[3] + 1, box_offset[0]:box_offset[2] + 1])
+
+        return labels, boxes, masks
+
+
+
+    # def tiles_for_box(self, box):
+    #     return tiles_per_box(box, self.step, self.size, self.offset)
+    #
+    # def tiles_for_mask(self, box, mask):
+    #     tiles = self.tiles_for_box(box)
+    #     if len(tiles) == 0:
+    #         return None
+    #
+    #     for tile in tiles:
+    #         clipped, offset = get_clipped(tile, box)
+    #         x1, y1, x2, y2 = offset
+    #         mask[y1:y2 + 1, x1:x2 + 1]
+        # tile = np.array(get_tile(tiles[0], self.step, self.size, self.offset))
+        #
+        # clip, offsets = [box.copy() for _ in range(2)]
+        # clip[:2] = np.maximum(tile[:2], clip[:2]) - tile[:2]
+        # clip[2:] = np.minimum(tile[2:], clip[2:]) - tile[:2]
+        # offsets[:2] = tile[:2] + clip[:2] - box[:2]
+        # offsets[2:] = box[:2] + clip[2:] - clip[:2]
+        # # box_offsets[:2] = tile[:2] + box[:2] - box_offsets[:2]
+        # # box_offsets[2:] = box[2:] - box[:2] + box_offsets[:2]
+        #
+        # return tile, clip, offsets, box
+
+
+
+
+
 
 
 if __name__ == '__main__':
+    label_names = 'Core Diffuse Neuritic CAA'.split()
     vips_img_dir = '/gladstone/finkbeiner/steve/work/data/npsad_data/vivek/amy-def-mfg'
     json_dir = '/home/gryan/projects/qupath/annotations/amyloid'
     vips_img_names = ['09-063', '10-033']
@@ -147,6 +228,9 @@ if __name__ == '__main__':
     vips_img_fnames = [os.path.join(vips_img_dir, f'XE{vips_img_name}_1_AmyB_1.mrxs') for vips_img_name in vips_img_names]
     json_fnames = [os.path.join(json_dir, f'{vips_img_name}.json') for vips_img_name in vips_img_names]
 
+    step, size = [tuple([1024] * 2)] * 2
+
+    ds = JsonDataset(json_fnames[0], label_names, step=step, size=size)
 
 
     # @staticmethod
