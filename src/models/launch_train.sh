db291664a3f7b810ffcdf5866a596b875422f793
Monika Ahirwar <mahirwar@fb-gpu-compute01.gladstone.internal>
2022-12-22 15:28:13 -0800

    Updated launch_train

--- src/models/launch_train.sh
+++ src/models/launch_train.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-python train_kfold.py '/gladstone/finkbeiner/steve/work/data/npsad_data/monika/' '/gladstone/finkbeiner/steve/work/data/npsad_data/monika/Datasets/amyb_wsi/train' '/gladstone/finkbeiner/steve/work/data/npsad_data/monika/Datasets/amyb_wsi/test' 5
+python train.py '/gladstone/finkbeiner/steve/work/data/npsad_data/monika/' '/gladstone/finkbeiner/steve/work/data/npsad_data/vivek/Datasets/amyb_wsi/train' '/gladstone/finkbeiner/steve/work/data/npsad_data/vivek/Datasets/amyb_wsi/val'
 #python train.py '/mnt/new-nas/work/data/npsad_data/vivek/''/mnt/new-nas/work/data/npsad_data/vivek/Datasets/amyb_wsi/train' '/mnt/new-nas/work/data/npsad_data/vivek/Datasets/amyb_wsi/test'
