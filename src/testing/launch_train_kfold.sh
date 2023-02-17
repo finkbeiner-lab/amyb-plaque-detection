9dd19e1fd92474689604f344c27efdc8e4b59edd
monika.ahirwar <monika.ahirwar@gladstone.ucsf.edu>
2023-02-17 13:39:33 -0800

    updated k fold code

--- src/testing/launch_train_kfold.sh
+++ src/testing/launch_train_kfold.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-python train_kfold.py --base_dir '/gladstone/finkbeiner/steve/work/data/npsad_data/monika/' --dataset_train_location '/gladstone/finkbeiner/steve/work/data/npsad_data/vivek/Datasets/amyb_wsi/train/train1/' --dataset_test_location '/gladstone/finkbeiner/steve/work/data/npsad_data/vivek/Datasets/amyb_wsi/val/val1/' --k_list 2 --repeat 2
+python train_kfold.py --base_dir '/gladstone/finkbeiner/steve/work/data/npsad_data/monika/' --dataset_train_location '/gladstone/finkbeiner/steve/work/data/npsad_data/vivek/Datasets/amyb_wsi/train/train1/' --dataset_test_location '/gladstone/finkbeiner/steve/work/data/npsad_data/vivek/Datasets/amyb_wsi/val/val1/' --k_list '3 5 7 9' --repeat 5
