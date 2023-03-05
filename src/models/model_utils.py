Fixes to metric computation

src/models/model_utils.py
@@ -88,7 +88,7 @@ def evaluate(model, device, dataset, thresh=None, mask_thresh=None, label_names=
         pred = dict([(k, v.to(torch.device('cpu'))) for k, v in eval(model, device, image, thresh=thresh, mask_thresh=mask_thresh).items()])
         if viz is None or idx in viz:
             visualizations.append([show(image, t, label_names=label_names, label_colors=label_colors) for t in (pred, target)])
-        metrics.update([pred], [dict([(k, v.to(device)) for k, v in target.items()])])
+        metrics.update([pred], [target])
     return metrics.compute(), visualizations
 
 