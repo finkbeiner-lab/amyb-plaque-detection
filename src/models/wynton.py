b884c4c18b4106e649f81813ab253595d55a320b
Gennadi Ryan <gennadiryan@gmail.com>
2023-03-05 06:26:39 -0500

    Fixes to training loop

--- src/models/wynton.py
+++ src/models/wynton.py
@@ -14,35 +14,79 @@ import torchvision
 import data.datasets as datasets
 import models.rcnn_conf as rcnn_conf
 from models.modules.rcnn import RCNN, RCNNTransform
-from models.model_utils import train, eval, show
-
+from models.model_utils import train, eval, evaluate, show
 
 
 if __name__ == '__main__':
-    label_names = 'Core Diffuse Neuritic CAA'.split()
-    label_colors = 'red blue green black'.split()
+    model_conf = dict(
+        pretrained=True,
+        num_classes=len(label_names) + 1
+    )
+    optim_conf = dict(
+        lr=1e-3,
+        momentum=9e-1,
+        weight_decay=5e-4
+    )
+    train_conf = dict(
+        epochs=0,
+        ckpt_freq=1,
+        batch_size=2,
+        device=0,
+    )
+    data_conf = (lambda tile_size: dict(
+        labels=dict(
+            names='Core Diffuse Neuritic CAA'.split(),
+            colors='red green blue black'.split(),
+        ),
+        slides=dict(
+            train='09-028 10-033'.split(),
+            test='09-063'.split(),
+        ),
+        size=dict(
+            train=tuple([tile_size] * 2),
+            test=tuple([tile_size] * 2),
+        ),
+        step=dict(
+            train=tuple([tile_size // 2] * 2),
+            test=tuple([tile_size] * 2),
+        ),
+    ))(1024)
 
     slide_dir = '/gladstone/finkbeiner/steve/work/data/npsad_data/vivek/amy-def-mfg-test/'
     slide_name_fn = lambda name: os.path.join(slide_dir, f'XE{name}_1_AmyB_1.mrxs')
     json_dir = '/gladstone/finkbeiner/steve/work/data/npsad_data/gennadi/jsons/amyb/'
     json_name_fn = lambda name: os.path.join(json_dir, f'{name}.json')
 
-    tile_size = 1024
-    slides_train, slides_test = '09-028 10-033'.split(), '09-063'.split()
-    dsets_train, dsets_test = [[datasets.VipsJsonDataset(slide_name_fn(slide), json_name_fn(slide), label_names, step=tuple([tile_size // (2 if training else 1)] * 2), size=tuple([tile_size] * 2)) for slide in slides] for slides, training in ((slides_train, True), (slides_test, False))]
+    dsets_train, dsets_test = [[datasets.VipsJsonDataset(
+        slide_name_fn(slide),
+        json_name_fn(slide),
+        data_conf['labels']['names'],
+        size=data_conf['size'][train],
+        step=data_conf['step'][train],
+    ) for slide in data_conf['slides'][train]] for train in 'train test'.split()]
     dset_train, dset_test = [torch.utils.data.ConcatDataset(dsets) for dsets in (dsets_train, dsets_test)]
-    loader = torch.utils.data.DataLoader(dset_train, batch_size=2, shuffle=True, collate_fn=lambda _: tuple(zip(*_)))
+    loader = torch.utils.data.DataLoader(dset_train, batch_size=train_conf['batch_size'], shuffle=True, collate_fn=lambda _: tuple(zip(*_)))
 
-    device = torch.device('cuda', 0)
-    epochs = 40
-    freq = 5
-    optim_config = dict(lr=1e-3, momentum=9e-1, weight_decay=5e-4)
-
-    model_conf = rcnn_conf.rcnn_v2_conf(pretrained=True, num_classes=5)
-    model = model_conf.module(
+    device = torch.device('cuda', train_conf['device'])
+    model = rcnn_conf.rcnn_v2_conf(**model_conf).module(
         # freeze_submodules=['backbone.body.conv1', 'backbone.body.bn1', 'backbone.body.layer1'],
         skip_submodules=['roi_heads.box_predictor', 'roi_heads.mask_predictor.mask_fcn_logits']
     ).to(device)
     optimizer = torch.optim.SGD(model.parameters(), **optim_config)
 
-    # train(model, optimizer, device, loader, epoch=1, progress=False)
+    for epoch in range(1, train_conf['epochs'] + 1):
+        train(model, optimizer, device, loader, epoch=epoch)
+
+        if epoch == train_conf['epochs'] or epoch % train_conf['ckpt_freq'] == 0:
+            metrics, visualizations = evaulate(
+                model,
+                device,
+                *zip(*[image, target for image, target in dset_test]),
+                label_names=data_conf['labels']['names'],
+                label_colors=data_conf['labels']['names'],
+                viz=list(range(min(16, len(dset_test)))),
+            )
+            print(metrics)
+            print()
+
+            visualization = torchvision.utils.make_grid([torchvision.utils.make_grid(_, nrow=1, padding=2) for _ in visualizations], nrow=sqrt(2 * len(visualizations)), padding=4)
