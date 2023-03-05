0b429922481fe4b3d5903dff501f47158dfda9f9
Gennadi Ryan <gennadiryan@gmail.com>
2023-03-05 06:55:49 -0500

    Fixes to metric computation

--- src/models/model_utils.py
+++ src/models/model_utils.py
@@ -85,10 +85,10 @@ def evaluate(model, device, dataset, thresh=None, mask_thresh=None, label_names=
     metrics = MAP(box_format='xyxy', iou_type='segm', bbox_area_ranges=area_ranges)
     visualizations = list()
     for idx, (image, target) in enumerate(dataset):
-        pred = eval(model, torch.device('cpu'), image, thresh=thresh, mask_thresh=mask_thresh)
+        pred = dict([(k, v.detach().to(torch.device('cpu'))) for k, v in eval(model, device, image, thresh=thresh, mask_thresh=mask_thresh).items()])
+        metrics.update([pred], [target])
         if viz is None or idx in viz:
             visualizations.append([show(image, t, label_names=label_names, label_colors=label_colors) for t in (pred, target)])
-        metrics.update([pred], [target])
     return metrics.compute(), visualizations
 
 
