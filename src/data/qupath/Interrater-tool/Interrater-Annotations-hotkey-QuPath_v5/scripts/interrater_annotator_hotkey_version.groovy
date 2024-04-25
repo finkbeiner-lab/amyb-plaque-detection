9d4358554de0ae0ac7ae61e55eb6842c6f6ed601
monika-ahirwar <117786920+monika-ahirwar@users.noreply.github.com>
2024-04-25 16:34:29 -0700

    Create interrater_annotator_hotkey_version.groovy

---
+++ src/data/qupath/Interrater-tool/Interrater-Annotations-hotkey-QuPath_v5/scripts/interrater_annotator_hotkey_version.groovy
@@ -0,0 +1,372 @@
+import groovy.transform.InheritConstructors
+import qupath.lib.gui.scripting.QPEx
+import qupath.lib.gui.tools.GuiTools
+import qupath.lib.objects.*
+import qupath.lib.objects.classes.*
+import qupath.lib.objects.hierarchy.*
+import qupath.lib.objects.hierarchy.events.*
+
+import javafx.application.Platform
+import javafx.concurrent.Task
+import javafx.scene.control.Alert
+import javafx.scene.control.Alert.AlertType
+import javafx.scene.control.ButtonType
+import javafx.scene.control.Dialog
+import javafx.scene.control.DialogPane
+import javafx.scene.control.ChoiceBox
+import javafx.scene.control.ListView;
+import javafx.scene.control.ListCell;
+
+import javafx.scene.control.Label
+import javafx.scene.control.TextArea
+import javafx.scene.control.TextField
+import javafx.scene.input.KeyCombination
+import javafx.scene.layout.GridPane
+import javafx.util.Callback
+import javafx.util.StringConverter
+
+import java.util.concurrent.Callable
+import java.util.concurrent.FutureTask
+
+import qupath.lib.roi.ROIs
+import qupath.lib.roi.EllipseROI;
+import qupath.lib.objects.PathDetectionObject
+import qupath.lib.geom.Point2
+import static qupath.lib.gui.scripting.QPEx.*
+import qupath.lib.objects.PathObjects
+import qupath.lib.regions.ImagePlane
+
+import qupath.lib.objects.PathObjects
+import qupath.lib.objects.PathAnnotationObject
+import qupath.lib.roi.RoiTools
+import qupath.lib.geom.Point2
+import qupath.lib.roi.EllipseROI
+import qupath.lib.gui.prefs.PathPrefs
+import qupath.lib.common.ColorTools
+import qupath.lib.objects.classes.PathClassFactory
+import qupath.lib.gui.QuPathGUI
+import qupath.lib.gui.tools.GuiTools
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.scene.input.KeyCode;
+
+
+
+/// For adding classes and Keys, chnage just here 
+// Define class and Keys
+class_names =  ["Cored", "Diffuse", "Coarse-Grained", "Cotton-Wool", "Burned-Out","None"]
+key_names = ["Ctrl+C", "Ctrl+F",  "Ctrl+G", "Ctrl+W",  "Ctrl+B","Ctrl+D"]
+color_list = [ [0, 255, 255],[255, 153, 102],[150, 79, 239],[160,170,90],[255,0,125],[124,125,255],[0,0,0]]
+
+
+abstract class Param<S, T> {
+    abstract T param
+    String paramName
+    Label paramLabel
+
+    def Param(String paramName, String paramLabel) {
+        this.paramName = paramName
+        this.paramLabel = new Label(paramLabel)
+    }
+
+    abstract void build()
+    abstract S get()
+    abstract void set(S value)
+}
+
+@InheritConstructors
+class TextParam extends Param<String, TextField> {
+    void build() { this.param = new TextField() }
+    String get() { return this.param.getText() }
+    void set(String value) { this.param.setText(value) }
+    void setEditable(Boolean editable) { this.param.setEditable(editable) }
+}
+
+@InheritConstructors
+class TextAreaParam extends Param<String, TextArea> {
+    void build() { this.param = new TextArea() }
+    String get() { return this.param.getText() }
+    void set(String value) { this.param.setText(value) }
+    void setEditable(Boolean editable) { this.param.setEditable(editable) }
+}
+
+@InheritConstructors
+class ChoiceParam<T> extends Param<T, ListView<T>> {
+    void build() { this.param = new ListView<T>() }
+    //T get() { return this.param.getValue() }
+    //void set(T value) { this.param.setValue(value) }
+    //void setConverter(StringConverter<T> value) { this.param.setConverter(value) }
+     T get() { return this.param.getSelectionModel().getSelectedItem(); }
+    void set(T value) { this.param.getSelectionModel().select(value); }
+    void setConverter(StringConverter<T> value) { 
+        this.param.setCellFactory(listView -> new ListCell<T>() {
+            @Override
+            protected void updateItem(T item, boolean empty) {
+                super.updateItem(item, empty);
+                setText(empty ? null : value.toString(item));
+            }
+        });
+    }
+    void setItems(List<T> values) { this.param.getItems().setAll(values); }
+    //void setItems(List<T> values) { this.param.getItems().setAll(values) }
+    void setItems(LinkedHashMap<String, T> values) {
+        this.setConverter(new StringConverter<T>() {
+            @Override
+            public String toString(T object) {
+                return values.entrySet().stream()
+                        .filter(entry -> Objects.equals(entry.getValue(), object))
+                        .map(Map.Entry::getKey)
+                        .findFirst()
+                        .orElse(null);
+            }
+
+            @Override
+            public T fromString(String string) {
+                return values.get(string);
+            }
+        });
+        this.setItems(new ArrayList<>(values.values()));
+    }
+}
+
+
+abstract class ParamDialog<P, R> implements Callable {
+    // List<Param<P, Object>> params
+    P params
+    GridPane gridPane
+    DialogPane dialogPane
+    Callback<ButtonType, R> resultConverter
+    Callable<Optional<R>> dialogCallable
+
+    // def ParamDialog(List<Param<R, Object>> params) {
+    def ParamDialog(P params) {
+        this.params = params
+    }
+
+    def ParamDialog() {
+        this.params = null
+    }
+
+    void buildParams() {
+        this.params.each({it.build()})
+    }
+
+    void buildGridPane() {
+        this.gridPane = new GridPane()
+        this.gridPane.setHgap(10)
+        this.gridPane.setVgap(10)
+        this.params.eachWithIndex({it, index -> this.gridPane.addRow(index, it.paramLabel, it.param)})
+    }
+
+    void buildDialogPane() {
+        this.dialogPane = new DialogPane()
+        this.dialogPane.getButtonTypes().setAll(ButtonType.OK, ButtonType.CANCEL)
+        this.dialogPane.setContent(this.gridPane)
+    }
+
+    // Not implemented
+    abstract void buildResultConverter()
+
+    void buildDialogCallable() {
+        this.dialogCallable = new Callable<Optional<R>>() {
+            @Override Optional<R> call() {
+                Dialog<R> dialog = new Dialog<R>()
+                //dialog.setDialogPane(ParamDialog.this.dialogPane)
+                dialog.setResultConverter(ParamDialog.this.resultConverter)
+                return dialog.showAndWait()
+            }
+        }
+    }
+
+    // Not implemented
+    abstract Callable<Optional<R>> build()
+
+    Optional<R> call() {
+        return GuiTools.callOnApplicationThread(this.build())
+    }
+
+    Optional<R> callAndWait() {
+        FutureTask<Optional<R>> future = new FutureTask<Optional<R>>(this.build())
+        Platform.runLater(future)
+        return future.get()
+    }
+}
+
+
+class AnnotatorDialog implements Runnable {
+    ArrayList<Param<ChoiceParam<Object>, Object>> params
+    Callable<PathClass> annotatorCallable
+    Callable<Optional> objectOrdering
+    ParamDialog<ArrayList<Param<ChoiceParam<Object>, Object>>, ArrayList<Object>> dialog
+    def imageData = getCurrentImageData()
+    def cuurentHierarchy = imageData.getHierarchy()
+    PathObjectHierarchy hierarchy = cuurentHierarchy
+    String key
+    String assignclass
+    def AnnotatorDialog(PathObjectHierarchy hierarchy, String key, String assignclass) {
+        this.hierarchy = hierarchy
+        this.key = key
+        this.assignclass = assignclass
+        LinkedHashMap<String, PathClass> pathClassMap = new LinkedHashMap<String, PathClass>()
+        pathClassMap.put("", PathClassFactory.getPathClassUnclassified())
+        this.params = new ArrayList<Param<ChoiceParam<Object>, Object>>()
+        params.add(new ChoiceParam<PathClass>("class", "Classification: ") {
+            @Override void build() {
+                super.build()
+                this.setItems(pathClassMap)
+            }
+        })
+
+        this.dialog = new ParamDialog<ArrayList<Param<ChoiceParam<Object>, Object>>, ArrayList<Object>>(params) {
+            @Override void buildResultConverter() {
+                this.resultConverter = new Callback<ButtonType, ArrayList<Object>>() {
+                    @Override ArrayList<Object> call(ButtonType param) {
+                        if (param == ButtonType.OK) {
+                            return AnnotatorDialog.this.params.collect({it.get()})
+                        }
+                        return null
+                    }
+                }
+            }
+
+            @Override Callable<Optional<ArrayList<Object>>> build() {
+                return this.dialogCallable
+            }
+        }
+    }
+
+    Callable<Optional<ButtonType>> buildAlertCallable(String msg) {
+        return new Callable<Optional<ButtonType>>() {
+            @Override Optional<ButtonType> call() {
+                Alert alert = new Alert(AlertType.INFORMATION)
+                alert.setContentText(msg)
+                return alert.showAndWait()
+            }
+        }
+    }
+
+
+
+
+    PathClass setObjectClass(PathObject object) {
+        if (this.key=="Ctrl+D") {
+           PathClass pathClass = PathClassFactory.getPathClass(this.assignclass)
+           removeObject(object, true)
+           def imageData = getCurrentImageData()
+           def name = getCurrentImageName()
+           def cuurentHierarchy = imageData.getHierarchy()
+           def annotations = imageData.getHierarchy().getAnnotationObjects()
+           for (int i = 0; i < annotations.size(); i++) {
+                annotations[i].setName(i +":"+ annotations[i].getPathClass())
+            }
+           resolveHierarchy()
+           def overlayOptions = getCurrentViewer().getOverlayOptions()
+           overlayOptions.setShowNames(true)
+           def allannotations = cuurentHierarchy.getAnnotationObjects()
+           def path = PathPrefs.userPathProperty().get()+"/exports/" +  name + ".geojson"
+           exportObjectsToGeoJson(allannotations,path,"PRETTY_JSON","FEATURE_COLLECTION")
+           return pathClass
+       }
+      else{ 
+        PathClass pathClass = PathClassFactory.getPathClass(this.assignclass)
+        if (pathClass != null) {
+            object.setPathClass(pathClass)
+        }
+       
+       def pointroi =  object.getROI()
+       def x = pointroi.getBoundsX()
+       def y = pointroi.getBoundsY()
+       def size = 20
+       def imageData = getCurrentImageData()
+       def name = getCurrentImageName()
+       def cuurentHierarchy = imageData.getHierarchy()
+       def annotations = imageData.getHierarchy().getAnnotationObjects()
+       for (int i = 0; i < annotations.size(); i++) {
+           annotations[i].setName(i +":"+ annotations[i].getPathClass())
+        }
+       //int itr = annotations.size();
+       //object.setName(itr +":"+ pathClass.toString())
+       resolveHierarchy()
+       def overlayOptions = getCurrentViewer().getOverlayOptions()
+       overlayOptions.setShowNames(true)
+       def allannotations = cuurentHierarchy.getAnnotationObjects()
+       def path = PathPrefs.userPathProperty().get()+"/exports/" +  name + ".geojson"
+       exportObjectsToGeoJson(allannotations,path,"PRETTY_JSON","FEATURE_COLLECTION")
+       return pathClass
+       }
+    }
+    
+    
+    
+
+    void buildAnnotatorCallable() {
+        this.annotatorCallable = new Callable<PathClass>() {
+            @Override PathClass call() {
+               PathObjectSelectionModel selectionModel = AnnotatorDialog.this.hierarchy.getSelectionModel()             
+               PathObject selected = selectionModel.getSelectedObject()
+               PathClass result = AnnotatorDialog.this.setObjectClass(selected)
+               return result
+               }         
+        }
+    }
+
+     
+
+
+
+
+
+
+
+    Callable<PathClass> build() {
+        this.buildAnnotatorCallable()
+        return this.annotatorCallable
+    }
+    
+    
+
+    @Override void run() {
+        this.build().call()
+    }
+}
+
+
+
+def build(String keyCombination=null, String assignclass="") {
+    def gui = QPEx.getQuPath().getInstance()
+    def hier = QPEx.getCurrentHierarchy()
+    def app = new AnnotatorDialog(hier, keyCombination, assignclass)
+    def menu = gui.installCommand(install_command_msg, app)
+    print gui.viewer
+    if (keyCombination != null) {
+        if (key_names.contains(keyCombination)) {
+                 Platform.runLater( {
+                    def scene = getQuPath().getStage().getScene()
+                    scene.getAccelerators().put(KeyCombination.keyCombination(keyCombination), app);
+                 })
+        }
+     }
+    return menu
+}
+
+
+
+def dictionary = [:]
+
+
+for (int i = 0; i < class_names.size(); i++) {
+    dictionary[class_names[i]] = key_names[i]
+}
+
+install_command_msg = dictionary.toString()
+
+
+dictionary.each { key, value ->
+    println("Key: $key, Value: $value")
+   def menu = build("$value","$key")
+}
+//def menu =  build("CTRL+E","None")
+
+
+
+
