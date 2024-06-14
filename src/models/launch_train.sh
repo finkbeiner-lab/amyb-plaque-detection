91abb46289a96f0a9c9ac2a848a3943a922203b7
monika.ahirwar <monika.ahirwar@gladstone.ucsf.edu>
2024-06-14 12:58:39 -0700

    updated interrater study, spllit data codes

--- src/models/launch_train.sh
+++ src/models/launch_train.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-python train.py '/gladstone/finkbeiner/steve/work/data/npsad_data/monika/' '/gladstone/finkbeiner/steve/work/data/npsad_data/vivek/Datasets/amyb_wsi/train' '/gladstone/finkbeiner/steve/work/data/npsad_data/vivek/Datasets/amyb_wsi/val'
+python3 train.py '/gladstone/finkbeiner/steve/work/data/npsad_data/vivek/' '/gladstone/finkbeiner/steve/work/data/npsad_data/vivek/Datasets/amyb_wsi/train' '/gladstone/finkbeiner/steve/work/data/npsad_data/vivek/Datasets/amyb_wsi/val'
 #python train.py '/mnt/new-nas/work/data/npsad_data/vivek/''/mnt/new-nas/work/data/npsad_data/vivek/Datasets/amyb_wsi/train' '/mnt/new-nas/work/data/npsad_data/vivek/Datasets/amyb_wsi/test'
