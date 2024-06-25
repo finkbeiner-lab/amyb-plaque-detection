8498f694326064f9ce2fcf11f74587d38cbb65df
monika.ahirwar <monika.ahirwar@gladstone.ucsf.edu>
2024-06-25 22:18:56 +0000

    updated codes from runpod

---
+++ src/models/launch_train_runpod.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+python3 train.py '/workspace/Projects/Amyb_plaque_detection/Datasets' '/workspace/Projects/Amyb_plaque_detection/Datasets/train_v2' '/workspace/Projects/Amyb_plaque_detection/Datasets/val_v2'
+#python train.py '/mnt/new-nas/work/data/npsad_data/vivek/''/mnt/new-nas/work/data/npsad_data/vivek/Datasets/amyb_wsi/train' '/mnt/new-nas/work/data/npsad_data/vivek/Datasets/amyb_wsi/test'
